d4323df7543f240e428700fe97939ccfd2f62391
Juan Camilo Soto Pineda <juancamilo.soto@outlook.com>
2023-07-19 20:42:18 -0500

    Work Advance

--- resources/views/angular.blade.php
+++ resources/views/angular.blade.php
@@ -279,7 +279,7 @@
 
 <body class="mat-typography bg-light">
     <app-root></app-root>
-    <script src="runtime.bbb3b2324539e36f.js" type="module"></script>
+    <script src="runtime.6fe4e54725c1f46e.js" type="module"></script>
     <script src="polyfills.c06e5487760266d8.js" type="module"></script>
     <script src="main.56b6125fbd4705ac.js" type="module"></script>
 
