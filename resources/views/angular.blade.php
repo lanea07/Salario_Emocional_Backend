c63e92afe9ad5a7e7ad41db32d603b911dd8f3a7
Juan Camilo Soto Pineda <juancamilo.soto@outlook.com>
2023-07-19 19:25:39 -0500

    Work Advance

--- resources/views/angular.blade.php
+++ resources/views/angular.blade.php
@@ -3,6 +3,8 @@
 
 <head>
     <meta charset="utf-8">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
     <title>Salario Emocional</title>
     <base href="/assets/angular/">
     <meta name="viewport" content="width=device-width, initial-scale=1">
