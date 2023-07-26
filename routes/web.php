1fb248469f0d2e951b30374e3d185afde53ea622
Juan Camilo Soto Pineda <juancamilo.soto@outlook.com>
2023-07-25 22:27:06 -0500

    Work Advance

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\AngularController;
+use App\Mail\BenefitUserCreated;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
@@ -23,9 +25,11 @@ Route::get('{any}', [AngularController::class, 'index'])->where('any', '^(?!api)
 // });
 
 // Route::get('/sendmail', function (Request $request) {
-//     $ip = $request->ip();
-//     Mail::raw('Hi user, a new login into your account from the IP Address: ' . $ip, function ($message) {
-//         $message->from('lanea07@gmail.com', 'Juan Soto');
-//         $message->to('juan.soto@flamingo.com', 'User Name');
-//     });
+    // $ip = $request->ip();
+    // Mail::raw('Hi user, a new login into your account from the IP Address: ' . $ip, function ($message) {
+    //     $message->from('lanea07@gmail.com', 'Juan Soto');
+    //     $message->to('juan.soto@flamingo.com', 'User Name');
+    // });
+    // Mail::to('juancamilo.soto@outlook.com')->send(new BenefitUserCreated(User::find(1)));
+    // return response('{message: sent}');
 // });
